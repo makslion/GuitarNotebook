a1c96e0e1c8fecbd7a5877422b3de88b6a3bc54b
makslion <247250195@mail.ru>
2019-04-25 08:03:26 +0100

    navigation

--- app/src/main/java/com/maksym/guitarnotebook/Drawer_layout.java
+++ app/src/main/java/com/maksym/guitarnotebook/Drawer_layout.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
 public class Drawer_layout extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
